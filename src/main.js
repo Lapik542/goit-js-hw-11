6345e6b1b84a657ad655e89c7f24e96576752a9b
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-01-30 19:43:40 +0200

    main

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector('.form');
 const gallery = document.querySelector('.gallery-js');
-
+const loader = document.querySelector('.loader');
+loader.style.display = 'none';
 
 form.addEventListener("submit", (eve) => {
     eve.preventDefault();
@@ -14,56 +15,54 @@ form.addEventListener("submit", (eve) => {
     const QUERY = form.querySelector('.input-js').value;
     const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&q=${QUERY}&image_type=photo&orientation=horizontal&safesearch=true`;
 
+    loader.style.display = 'block';
+
     fetch(BASE_URL)
     .then(res => {
         if (!res.ok) {
-            throw new Error('Network response was not ok');
+            iziToast.error({
+                title: 'Error',
+                message: error.message
+            });
+            loader.style.display = 'none';
         }
         return res.json();
     })
     .then(data => {
-        if (data.hits.length === 0) {
-            throw new Error('No images found for the provided query');
-        }
-
-        // Render images in the gallery
         gallery.innerHTML = data.hits.reduce((html, image) => html + `
-            <li class="gallery-item">
-                <a class="gallery-link" href="${image.largeImageURL}">
-                    <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" />
-                </a>
-                <ul class="photo-list">
-                    <li class="photo-list-value">
-                        <h2 class="photo-name">Likes</h2>
-                        <p class="photo-value">${image.likes}</p>
-                    </li>
-                    <li>
-                        <h2 class="photo-name">Views</h2>
-                        <p class="photo-value">${image.views}</p>
-                    </li>
-                    <li>
-                        <h2 class="photo-name">Comments</h2>
-                        <p class="photo-value">${image.comments}</p>
-                    </li>
-                    <li>
-                        <h2 class="photo-name">Downloads</h2>
-                        <p class="photo-value">${image.downloads}</p>
-                    </li>
-                </ul>
+        <li class="gallery-item">
+        <a class="gallery-link" href="${image.largeImageURL}">
+            <img class="gallery-image" src="${image.webformatURL}" alt="${image.tags}" />
+        </a>
+        <ul class="photo-list">
+            <li class="photo-list-value">
+                <h2 class="photo-name">Likes</h2>
+                <p class="photo-value">${image.likes}</p>
             </li>
+            <li>
+                <h2 class="photo-name">Views</h2>
+                <p class="photo-value">${image.views}</p>
+            </li>
+            <li>
+                <h2 class="photo-name">Comments</h2>
+                <p class="photo-value">${image.comments}</p>
+            </li>
+            <li>
+                <h2 class="photo-name">Downloads</h2>
+                <p class="photo-value">${image.downloads}</p>
+            </li>
+        </ul>
+    </li>
         `, '');
 
-        // Initialize the lightbox
         const lightbox = new SimpleLightbox('.gallery a', {
             captionsData: 'alt',
             captionDelay: 250,
         });
+
+        loader.style.display = 'none';
     })
-    .catch(error => {
-        iziToast.error({
-            title: 'Error',
-            message: error.message
-        });
-    });
 
 });
+
+
